3ab188bf8398eeffd3584a16dbf3f8e55d105b55
Andrew Farabow <aafarabow@gmail.com>
2022-03-27 17:36:30 -0400

    remove CUT and add bash script

---
+++ train_fx_simple.sh
@@ -0,0 +1,6 @@
+python pytorch-CycleGAN-and-pix2pix/train.py \
+    --dataroot data/movie_style \
+    --name SW_CycleGAN \
+    --model cycle_gan \
+    --netG resnet_6blocks \
+    --crop_size 128
