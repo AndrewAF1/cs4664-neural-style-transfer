5e418f950d87ea5eb989eb50ee41f5ff90b22c11
Andrew Farabow <aafarabow@vt.edu>
2022-04-22 22:06:05 -0400

    stuff I forgot

--- train_fx_simple.sh
+++ train_fx_simple.sh
@@ -1,6 +1,7 @@
-python pytorch-CycleGAN-and-pix2pix/train.py \
+python3 pytorch-CycleGAN-and-pix2pix/train.py \
     --dataroot data/movie_style \
     --name SW_CycleGAN \
     --model cycle_gan \
     --netG resnet_6blocks \
-    --crop_size 128
+    --crop_size 128 \
+    --continue_train
