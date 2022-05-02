f348ddd3d9cea43e8befa5954ec66a73a68bfabf
Aditi Diwan <aditid@fermi.rlogin>
2022-05-02 12:51:45 -0400

    lr hyperparameter results

--- tune_test.py
+++ tune_test.py
@@ -9,11 +9,19 @@ import os
 
 #     os.system(cmd)
 
-epochs = [(2,8), (4, 6), (5, 5), (6, 4), (8, 2)]
-
-for n_epochs, n_epochs_decay in epochs:
-    cmd = "python3 pytorch-CycleGAN-and-pix2pix/test.py --dataroot data/pix2pix/ncdataset --name ncd_pix2pix_epochs_{}_{} --model colorization".format(
-        n_epochs, n_epochs_decay
+learning_rates = [0.00002, 0.0001, 0.0002, 0.0005]
+for lr in learning_rates:
+    cmd = "python3 pytorch-CycleGAN-and-pix2pix/test.py --dataroot data/pix2pix/ncdataset  --name ncd_pix2pix_lr_{} --model colorization".format(
+        lr
     )
 
     os.system(cmd)
+
+# epochs = [(2,8), (4, 6), (5, 5), (6, 4), (8, 2)]
+
+# for n_epochs, n_epochs_decay in epochs:
+#     cmd = "python3 pytorch-CycleGAN-and-pix2pix/test.py --dataroot data/pix2pix/ncdataset --name ncd_pix2pix_epochs_{}_{} --model colorization".format(
+#         n_epochs, n_epochs_decay
+#     )
+
+#     os.system(cmd)
